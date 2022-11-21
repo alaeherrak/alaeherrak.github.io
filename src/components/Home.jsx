93e90f4d8b420325ba635253621629f86617a3ff
alae-herrak <alaeherrak@gmail.com>
2022-11-21 21:53:11 +0100

    home changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,17 @@ const Home = () => {
     <div className="home">
       <section className="hero" style={{background: `url(${Laptop})`}}>
         <div className="hero-container">
+          <div className="hero-container-left">
+            <div  className="hero-container-left-container">
+              <h2>Hi, I'm Alae,</h2>
+              <h1>Full Stack Web Developer</h1>
+            </div>
+          </div>
+          <div className="hero-container-right">
 
+          </div>
         </div>
       </section>
-      {/* <img src={Laptop} alt="laptop image" /> */}
     </div>
   );
 };
