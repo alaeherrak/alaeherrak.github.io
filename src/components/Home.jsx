bd3a826122395f7942a958d5d518c4a916ddaeb9
alae-herrak <alaeherrak@gmail.com>
2022-11-21 22:42:35 +0100

    made improvments to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,52 @@
+import { useState } from "react";
 import "../assets/home.css";
 import Laptop from "../assets/images/lightbulbs.jpg";
 
 const Home = () => {
+  const [fullnamePopup, setFullnamePopup] = useState("popupHide");
+  const [skillsPopup, setSkillsPopup] = useState("popupHide");
+
+
+  const handleFullnamePopupOver = () => {
+    setFullnamePopup("fullname");
+  };
+  const handleFullnamePopupOut = () => {
+    setFullnamePopup("popupHide");
+  };
+  const handleSkillsPopupOver = () => {
+    setSkillsPopup("skills");
+  };
+  const handleSkillsPopupOut = () => {
+    setSkillsPopup("popupHide");
+  };
+
   return (
     <div className="home">
-      <section className="hero" style={{background: `url(${Laptop})`}}>
+      <section className="hero" style={{ background: `url(${Laptop})` }}>
         <div className="hero-container">
           <div className="hero-container-left">
-            <div  className="hero-container-left-container">
-              <h2>Hi, I'm Alae,</h2>
-              <h1>Full Stack Web Developer</h1>
+            <div className="hero-container-left-container">
+              <div className="hero-container-left-div top">
+                <div className={fullnamePopup}>{'< fullname = "alae herrak" />'}</div>
+              </div>
+              <div style={{ textAlign: "center" }}>
+                <h1 onMouseOver={handleFullnamePopupOver} onMouseOut={handleFullnamePopupOut}>
+                  Hi, I'm <span>Alae</span>,
+                </h1>
+                <h3 onMouseOver={handleSkillsPopupOver} onMouseOut={handleSkillsPopupOut}>Full Stack Web Developer</h3>
+              </div>
+              <div className="hero-container-left-div bot">
+                <div className={skillsPopup}>
+                  <pre>
+                    {
+                      "let skills: {\n    frontEnd: true,\n    backEnd: true\n};"
+                    }
+                  </pre>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="hero-container-right">
-
-          </div>
+          <div className="hero-container-right"></div>
         </div>
       </section>
     </div>
