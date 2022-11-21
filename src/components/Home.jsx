c08c36dc7ac97e7764e33eefe246bca92285f661
alae-herrak <alaeherrak@gmail.com>
2022-11-21 21:48:53 +0100

    added hero section on home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,17 @@
 import "../assets/home.css";
+import Laptop from "../assets/images/lightbulbs.jpg";
 
 const Home = () => {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <section className="hero" style={{background: `url(${Laptop})`}}>
+        <div className="hero-container">
+
+        </div>
+      </section>
+      {/* <img src={Laptop} alt="laptop image" /> */}
+    </div>
+  );
 };
 
 export default Home;
