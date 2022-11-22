383e144adbcfad73280bbc4b7a61db30aa37465a
alae-herrak <alaeherrak@gmail.com>
2022-11-22 19:33:46 +0100

    home right part

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import "../assets/home.css";
 import LightBulbs from "../assets/images/lightbulbs.jpg";
+import Github from "../assets/images/github.png";
+import Linkedin from "../assets/images/linkedin.png";
 
 const Home = () => {
   const [fullnamePopup, setFullnamePopup] = useState("popupHide");
   const [skillsPopup, setSkillsPopup] = useState("popupHide");
-
 
   const handleFullnamePopupOver = () => {
     setFullnamePopup("fullname");
@@ -27,13 +28,23 @@ const Home = () => {
           <div className="hero-container-left">
             <div className="hero-container-left-container">
               <div className="hero-container-left-div top">
-                <div className={fullnamePopup}>{'< fullname = "alae herrak" />'}</div>
+                <div className={fullnamePopup}>
+                  {'< fullname = "alae herrak" />'}
+                </div>
               </div>
               <div style={{ textAlign: "center" }}>
-                <h1 onMouseOver={handleFullnamePopupOver} onMouseOut={handleFullnamePopupOut}>
+                <h1
+                  onMouseOver={handleFullnamePopupOver}
+                  onMouseOut={handleFullnamePopupOut}
+                >
                   Hi, I'm <span>Alae</span>,
                 </h1>
-                <h3 onMouseOver={handleSkillsPopupOver} onMouseOut={handleSkillsPopupOut}>Full Stack Web Developer</h3>
+                <h3
+                  onMouseOver={handleSkillsPopupOver}
+                  onMouseOut={handleSkillsPopupOut}
+                >
+                  Full Stack Web Developer
+                </h3>
               </div>
               <div className="hero-container-left-div bot">
                 <div className={skillsPopup}>
@@ -46,7 +57,16 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="hero-container-right"></div>
+          <div className="hero-container-right">
+            <div className="links">
+              <a href="https://www.github.com/alae-herrak">
+                <img src={Github} alt="github" width='70px'/>
+              </a>
+              <a href="https://www.linkedin.com/in/alae-herrak-ba9039210/">
+                <img src={Linkedin} alt="linkedin" width='70px'/>
+              </a>
+            </div>
+          </div>
         </div>
       </section>
     </div>
