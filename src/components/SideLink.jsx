bde1b8b770800dc874ebd2dbf825eb72b58c8f16
alaeherrak <112645834+alaeherrak@users.noreply.github.com>
2022-11-24 21:05:31 +0100

    Delete src directory

--- src/components/SideLink.jsx
+++
@@ -1,9 +0,0 @@
-const SideLink = ({ link, logo }) => {
-  return (
-    <a href={link} target="_blank">
-      <img src={logo} alt="sidelink" width="35px" />
-    </a>
-  );
-};
-
-export default SideLink;
